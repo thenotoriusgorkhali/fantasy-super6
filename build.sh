faa547d4771897422fcf1021f83eba5ea70a23bb
thenotoriusgorkhali <aaevashadhikari666@gmail.com>
2026-05-08 21:44:49 +0545

    fix admin creation for PostgreSQL

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth.models import User
-User.objects.filter(username='admin').delete()
-User.objects.create_superuser('admin', 'admin@fantasy.com', 'admin123')
-"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@fantasy.com', 'admin123')"
